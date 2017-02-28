1d0754eec0435cb6dc7b6728bf4dd8c7ce2bd957
Ashley Jain <ashleyjain@MacBook-Air.local>
2017-03-01 02:07:12 +0530

    q1_a completed

--- q1_a.m
+++ q1_a.m
@@ -1,14 +1,13 @@
 % clc;clear;
 % 
-% 
-% trainf = fopen('r8-train-all-terms.txt','r');
-% 
 % catg_map = containers.Map;
 % dict_map = containers.Map;
 % catg_index=0;
 % word_index=0;
-% no_word_catg = ones(19983,8);
+% % no_word_catg = ones(19983,8);
+% no_word_catg = ones(23586,8);
 % 
+% trainf = fopen('r8-train-all-terms.txt','r');
 % row = fgetl(trainf);
 % while ischar(row)
 %     splits = strsplit(row);
@@ -28,19 +27,55 @@
 %     end
 %     row = fgetl(trainf);
 % end
-% count_totWords_catg = sum(no_word_catg)+length(keys(dict_map));
-% 
 % 
 % fclose(trainf);
+% 
+% 
+% testff = fopen('r8-test-all-terms.txt','r');
+% row = fgetl(testff);
+% while ischar(row)
+%     splits = strsplit(row);
+%     if(~isKey(catg_map,char(splits(1))))
+%         catg_index=catg_index+1;
+%         catg_map(char(splits(1)))=catg_index;
+%         no_catgDocs(catg_index)=1;
+%     else        
+%         no_catgDocs(catg_map(char(splits(1))))=no_catgDocs(catg_map(char(splits(1))))+1;
+%     end
+%     for i = 2:length(splits)
+%        if(~isKey(dict_map,char(splits(i))))
+%            word_index=word_index+1;
+%            dict_map(char(splits(i)))=word_index;
+%        end
+%        no_word_catg(dict_map(char(splits(i))),catg_map(char(splits(1))))=no_word_catg(dict_map(char(splits(i))),catg_map(char(splits(1))))+1;
+%     end
+%     row = fgetl(testff);
+% end
+% 
+% count_totWords_catg = sum(no_word_catg)+length(keys(dict_map));
+% fclose(testff);
+
+
+
+
+
+
+
+
+
+
+
+
 
 load('train')
 
-testf = fopen('r8-test-all-terms.txt','r');
-
 prob_allcatg=zeros(1,length(keys(catg_map)));
-row = fgetl(testf);
 acc=0;
 count_row=0;
+
+trainff = fopen('r8-train-all-terms.txt','r');
+row = fgetl(trainff);
+
 while ischar(row)
     splits = strsplit(row);
     prob_allcatg=prob_allcatg+log(no_catgDocs/sum(no_catgDocs));
@@ -56,9 +91,53 @@ while ischar(row)
         acc=acc+1;
     end
     count_row=count_row+1;
-    row = fgetl(trainf);
+    row = fgetl(trainff);
 end
-acc/count_row
+disp('accuracy over train set:');
+disp(acc/count_row);
+
+fclose(trainff);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+prob_allcatg=zeros(1,length(keys(catg_map)));
+acc=0;
+count_row=0;
+
+testf = fopen('r8-test-all-terms.txt','r');
+row = fgetl(testf);
+while ischar(row)
+    splits = strsplit(row);
+    prob_allcatg=prob_allcatg+log(no_catgDocs/sum(no_catgDocs));
+    for i=2:length(splits)
+        if(~isKey(dict_map,char(splits(i))))
+            no_wordCatg = ones(1,length(keys(catg_map)));
+        else
+            no_wordCatg = no_word_catg(dict_map(char(splits(i))),:);
+        end    
+         prob_allcatg=prob_allcatg+log( no_wordCatg./count_totWords_catg );
+    end
+    if(find(prob_allcatg==max(prob_allcatg))==catg_map(char(splits(1))))
+        acc=acc+1;
+    end
+    count_row=count_row+1;
+    row = fgetl(testf);
+end
+disp('accuracy over test set:');
+disp(acc/count_row);
 
 fclose(testf);
 
